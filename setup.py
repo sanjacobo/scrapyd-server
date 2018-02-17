f36f0f9ff014dc38f082fba9e94e98133b0cd492
Santiago Guichon <santiago.guichon@gmail.com>
2018-02-17 03:50:31 -0300

    scrapyd try

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="scrapyd",
+    version=read('VERSION'),
+    author="Santiago Guichon",
+    author_email="v-sguichon@expedia.com",
+    description="Scrapyd server",
+    # packages=['an_example_pypi_project', 'tests'],
+    install_requires=[
+        'scrapy',
+        'scrapyd',
+        'scrapyd-client',
+    ]
+)
